92b9cacde8ba99715da33d88f7d851a16e111e1b
denilson <91sbarahona@gmail.com>
2019-03-29 10:45:42 -0600

    Modificación donde se modificaron los archivos dentro de la capeta values, ademas de tener el diseño de la pantalla de direcciones.

--- app/src/main/java/com/example/ssis_tracker/view/home/HomeActivity.java
+++ app/src/main/java/com/example/ssis_tracker/view/home/HomeActivity.java
@@ -2,18 +2,17 @@ package com.example.ssis_tracker.view.home;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import com.example.ssis_tracker.adapter.*;
 import com.example.ssis_tracker.R;
 
 public class HomeActivity extends AppCompatActivity
@@ -26,20 +25,20 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        RecyclerView rvDirecciones = findViewById(R.id.rv_direcciones);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getApplicationContext());
+        adapter_direciones  adapter_direcciones = new adapter_direciones(this.getApplicationContext());
+
+        rvDirecciones.setAdapter(adapter_direcciones);
+        rvDirecciones.setLayoutManager(linearLayoutManager);
+
+        rvDirecciones.scheduleLayoutAnimation();
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
