e7480831528062bbd9f007182c3f6afb213e772a
denilson <91sbarahona@gmail.com>
2019-04-01 15:21:37 -0600

    se agregó la sección de comentarios en la pantalla de actividades, ademas se modificó el boton de pumpunear para tener un contador de esas acciones.

--- app/src/main/java/com/example/ssis_tracker/adapter/proyectos/AdapterMetas.java
+++ app/src/main/java/com/example/ssis_tracker/adapter/proyectos/AdapterMetas.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.example.ssis_tracker.R;
 import com.example.ssis_tracker.model.Meta;
 import com.github.mikephil.charting.charts.PieChart;
